903881ae8ad2494a27801a57b9c4c636873bcf25
Ghaniyat27 <ganiyat4real1@gmail.com>
2024-01-27 20:07:52 -0800

    Cocktail shaker sort task

---
+++ 101-cocktail_sort_list.c
@@ -0,0 +1,98 @@
+#include "sort.h"
+
+void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **shaker);
+void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker);
+void cocktail_sort_list(listint_t **list);
+
+/**
+ * swap_node_ahead - Swap a Node's in a listint_t doubly-linked list
+ * List Of Int With The Node Ahead Of It.
+ * @list: A Pointer To The Head of a Doubly-linked List of Int.
+ * @tail: A Pointer To The Tail of The Doubly-linked List.
+ * @shaker: A Pointer To The Current Swapping Node's of The Cocktail Shaker Algorithm.
+ */
+void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **shaker)
+{
+	listint_t *tmp = (*shaker)->next;
+
+	if ((*shaker)->prev != NULL)
+		(*shaker)->prev->next = tmp;
+	else
+		*list = tmp;
+	tmp->prev = (*shaker)->prev;
+	(*shaker)->next = tmp->next;
+	if (tmp->next != NULL)
+		tmp->next->prev = *shaker;
+	else
+		*tail = *shaker;
+	(*shaker)->prev = tmp;
+	tmp->next = *shaker;
+	*shaker = tmp;
+}
+
+/**
+ * swap_node_behind - Swap's a Node In The listint_t doubly-linked
+ * List of Int with the Node's Behind It.
+ * @list: A Pointer to the Head of a Doubly-linked List of Int.
+ * @tail: A Pointer to the Tail of The Doubly-linked List.
+ * @shaker: A Pointer to the Current Swapping Node's of The Cocktail Shaker Algorithm.
+ */
+void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker)
+{
+	listint_t *tmp = (*shaker)->prev;
+
+	if ((*shaker)->next != NULL)
+		(*shaker)->next->prev = tmp;
+	else
+		*tail = tmp;
+	tmp->next = (*shaker)->next;
+	(*shaker)->prev = tmp->prev;
+	if (tmp->prev != NULL)
+		tmp->prev->next = *shaker;
+	else
+		*list = *shaker;
+	(*shaker)->next = tmp;
+	tmp->prev = *shaker;
+	*shaker = tmp;
+}
+
+/**
+ * cocktail_sort_list - Sort's The listint_t Doubly-linked List of Int in
+ * Ascending Order Using the Cocktail Shaker Algorithm.
+ * @list: A Pointer to the Head of a Listint_t Doubly-linked List.
+ */
+void cocktail_sort_list(listint_t **list)
+{
+	listint_t *tail, *shaker;
+	bool shaken_not_stirred = false;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	for (tail = *list; tail->next != NULL;)
+		tail = tail->next;
+
+	while (shaken_not_stirred == false)
+	{
+		shaken_not_stirred = true;
+		for (shaker = *list; shaker != tail; shaker = shaker->next)
+		{
+			if (shaker->n > shaker->next->n)
+			{
+				swap_node_ahead(list, &tail, &shaker);
+				print_list((const listint_t *)*list);
+				shaken_not_stirred = false;
+			}
+		}
+		for (shaker = shaker->prev; shaker != *list;
+				shaker = shaker->prev)
+		{
+			if (shaker->n < shaker->prev->n)
+			{
+				swap_node_behind(list, &tail, &shaker);
+				print_list((const listint_t *)*list);
+				shaken_not_stirred = false;
+			}
+		}
+	}
+}
