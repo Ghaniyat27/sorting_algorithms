9a4323be13f88f1a877cd8da28b0c3bdfce3c68c
Ghaniyat27 <ganiyat4real1@gmail.com>
2024-01-27 20:19:56 -0800

    Counting Sort task

---
+++ 102-counting_sort.c
@@ -0,0 +1,68 @@
+#include "sort.h"
+
+/**
+ * get_max - Get the MAX Value in An Array of Integer.
+ * @array: An Array of Integer's.
+ * @size: The Size of An Array.
+ *
+ * Return: The MAX Int in the Array.
+ */
+int get_max(int *array, int size)
+{
+	int max, i;
+
+	for (max = array[0], i = 1; i < size; i++)
+	{
+		if (array[i] > max)
+			max = array[i];
+	}
+
+	return (max);
+}
+
+/**
+ * counting_sort - Sort The Array of Integer's in an Ascending Order
+ * Using The Counting Sort Algorithm.
+ * @array: An Array of Int.
+ * @size: The Size of An Array.
+ *
+ * Description: Print's the Counting Array's After Setting It Up.
+ */
+void counting_sort(int *array, size_t size)
+{
+	int *count, *sorted, max, i;
+
+	if (array == NULL || size < 2)
+		return;
+
+	sorted = malloc(sizeof(int) * size);
+	if (sorted == NULL)
+		return;
+	max = get_max(array, size);
+	count = malloc(sizeof(int) * (max + 1));
+	if (count == NULL)
+	{
+		free(sorted);
+		return;
+	}
+
+	for (i = 0; i < (max + 1); i++)
+		count[i] = 0;
+	for (i = 0; i < (int)size; i++)
+		count[array[i]] += 1;
+	for (i = 0; i < (max + 1); i++)
+		count[i] += count[i - 1];
+	print_array(count, max + 1);
+
+	for (i = 0; i < (int)size; i++)
+	{
+		sorted[count[array[i]] - 1] = array[i];
+		count[array[i]] -= 1;
+	}
+
+	for (i = 0; i < (int)size; i++)
+		array[i] = sorted[i];
+
+	free(sorted);
+	free(count);
+}
